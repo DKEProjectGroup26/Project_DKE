9d102c9749449e33fb761d02c6ca0748838adb95
Max-le <max.09@outlook.com>
2018-10-10 19:39:48 +0200

    randomColors generation

--- Brute.java
+++ Brute.java
@@ -3,85 +3,85 @@ import java.util.*;
 import java.util.Arrays; 
 import java.awt.Color;
 
-		class ColEdge
-			{
-			int u;
-			int v;
-			}
-		
+class ColEdge
+{
+	int u;
+	int v;
+}
+
 public class Brute
+{
+
+	public final static boolean DEBUG = true;
+
+	public final static String COMMENT = "//";
+
+	public static void main( String args[] )
+	{
+		if( args.length < 1 )
 		{
-		
-		public final static boolean DEBUG = true;
-		
-		public final static String COMMENT = "//";
-		
-		public static void main( String args[] )
-			{
-			if( args.length < 1 )
-				{
-				System.out.println("Error! No filename specified.");
-				System.exit(0);
-				}
+			System.out.println("Error! No filename specified.");
+			System.exit(0);
+		}
 
-				
-			String inputfile = args[0];
-			
-			boolean seen[] = null;
-			
+
+		String inputfile = args[0];
+
+		boolean seen[] = null;
+
 			//! n is the number of vertices in the graph
-			int n = -1;
-			
-			//! m is the number of edges in the graph
-			int m = -1;
-			
-			//! e will contain the edges of the graph
-			ColEdge e[] = null;
-			
-			try 	{ 
-			    	FileReader fr = new FileReader(inputfile);
-			        BufferedReader br = new BufferedReader(fr);
+		int n = -1;
 
-			        String record = new String();
-					
+			//! m is the number of edges in the graph
+		int m = -1;
+
+			//! e will contain the edges of the graph
+		ColEdge e[] = null;
+
+		try 	{ 
+			FileReader fr = new FileReader(inputfile);
+			BufferedReader br = new BufferedReader(fr);
+
+			String record = new String();
+
 					//! THe first few lines of the file are allowed to be comments, staring with a // symbol.
 					//! These comments are only allowed at the top of the file.
-					
+
 					//! -----------------------------------------
-			        while ((record = br.readLine()) != null)
-						{
-						if( record.startsWith("//") ) continue;
+			while ((record = br.readLine()) != null)
+			{
+				if( record.startsWith("//") ) continue;
 						break; // Saw a line that did not start with a comment -- time to start reading the data in!
-						}
-	
+					}
+
 					if( record.startsWith("VERTICES = ") )
-						{
+					{
 						n = Integer.parseInt( record.substring(11) );					
 						if(DEBUG) System.out.println(COMMENT + " Number of vertices = "+n);
-						}
+					}
 
 					seen = new boolean[n+1];	
-						
+
 					record = br.readLine();
 					
 					if( record.startsWith("EDGES = ") )
-						{
+					{
 						m = Integer.parseInt( record.substring(8) );					
 						if(DEBUG) System.out.println(COMMENT + " Expected number of edges = "+m);
-						}
+					}
 
 					e = new ColEdge[m];	
-												
+
 					for( int d=0; d<m; d++)
-						{
+					{
 						if(DEBUG) System.out.println(COMMENT + " Reading edge "+(d+1));
 						record = br.readLine();
 						String data[] = record.split(" ");
 						if( data.length != 2 )
-								{
-								System.out.println("Error! Malformed edge line: "+record);
-								System.exit(0);
-								}
+						{
+							System.out.println("Error! Malformed edge line: "+record);
+							System.exit(0);
+						}
 						e[d] = new ColEdge();
 						
 						e[d].u = Integer.parseInt(data[0]);
@@ -91,28 +91,28 @@ public class Brute
 						seen[ e[d].v ] = true;
 						
 						if(DEBUG) System.out.println(COMMENT + " Edge: "+ e[d].u +" "+e[d].v);
-				
-						}
-									
+
+					}
+
 					String surplus = br.readLine();
 					if( surplus != null )
-						{
+					{
 						if( surplus.length() >= 2 ) if(DEBUG) System.out.println(COMMENT + " Warning: there appeared to be data in your file after the last edge: '"+surplus+"'");						
-						}
-					
 					}
-			catch (IOException ex)
+					
+				}
+				catch (IOException ex)
 				{ 
 		        // catch possible io errors from readLine()
-			    System.out.println("Error! Problem reading file "+inputfile);
-				System.exit(0);
+					System.out.println("Error! Problem reading file "+inputfile);
+					System.exit(0);
 				}
 
-			for( int x=1; x<=n; x++ )
+				for( int x=1; x<=n; x++ )
 				{
-				if( seen[x] == false )
+					if( seen[x] == false )
 					{
-					if(DEBUG) System.out.println(COMMENT + " Warning: vertex "+x+" didn't appear in any edge : it will be considered a disconnected vertex on its own.");
+						if(DEBUG) System.out.println(COMMENT + " Warning: vertex "+x+" didn't appear in any edge : it will be considered a disconnected vertex on its own.");
 					}
 				}
 
@@ -146,10 +146,26 @@ public class Brute
 				}
 				System.out.println("Done.");
 
-				if (DEBUG) System.out.println(Arrays.deepToString(vertices));
+
+				//Create a list with x random colors ( x is the current chromatic number's value)
+				int[] colors = new int[chromatic_number];
+				for (int i = 0; i<colors.length-1; i++){
+					colors[i] = randomNumber();
+
+				}
+
+				//Assign a color to each vertex
+				for (int i = 0; i < n ; i++){
+					
+				}
 				
-			
-		}
+				if (DEBUG) System.out.println(Arrays.deepToString(vertices));
+
+
+
+				
+
+			}
 
 		/**Returns a random color.
 		@param
@@ -167,12 +183,21 @@ public class Brute
 			Color color1 = new Color(r,g,b);
 
 			if (DEBUG){ 
-			System.out.println("Colors : "+r);
-			System.out.println(g);
-			System.out.println(b);
-			System.out.println(color1.toString());
+				System.out.println("Colors : "+r);
+				System.out.println(g);
+				System.out.println(b);
+				System.out.println(color1.toString());
 			} return color1; 
 
 		}
+		/**
+		@return a random integer.
+		**/
+		public static int randomNumber(){
 
-}
+			Random rand = new Random();
+			return rand.nextInt();
+
+		}
+
+	}
